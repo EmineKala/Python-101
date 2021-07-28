b1ec6b40f6a0c5b33694a380ac4166f676d1c49e
EmineKala <69795798+EmineKala@users.noreply.github.com>
2021-07-28 16:47:15 +0300

    Python

--- Python/Lab Saatleri/laörnek3-2.py
+++ Python/Lab Saatleri/laörnek3-2.py
@@ -1,10 +1,9 @@
-
 def first():
 
  print("Çizilebilecek Şekiller: \n1) Dik Üçgen \n2) Eşkenar Üçgen \n3) Kare \n4) Dikdörtgen \n5) Daire ")
 
  count = int(input ("Kaç Şekil Çizmek İstiyorsunuz?: "))
- 
+
  while count:
     number = input ("Çizmek istediğiniz şeklin kodunu giriniz: ")
     draw(number)
@@ -12,42 +11,43 @@ def first():
 
 
 def draw(number):
-    r=0
+
 
     if number == "1":
 
        rows = int(input("Dik üçgen için yükseklik değeri giriniz: "))
-       while r <= rows:
-         print ("*"*r)
-         r = r+1
+       for i in range(1,rows+1):
+         print ("*"*i)
+
 
     elif number == "2":
         rows = int(input("Eşkenar üçgen için yükseklik değeri giriniz: "))
-        while r <= rows:
-         print ("   " * (rows - r) + " * " * (2*r - 1))
-         r = r+1
+        for i in range(1,rows+1):
+         print (" " * (rows - i) + "*" * (2*i - 1))
+
 
     elif number == "3":
         rows = int(input("Karenin bir kenar uzunluğu için değer giriniz: "))
-        print()
-        while r < rows:
-          print (" * " * rows)
-          r = r+1
+
+        for i in range(1,rows+1):
+          print ("*" * rows)
+
 
     elif number == "4":
         rows = int(input("Dikdörtgen için yükseklik değeri giriniz: "))
         columns = int(input("Dikdörtgen için genişlik değeri giriniz: "))
-        while r < rows:
-          print (" * " * columns)
-          r = r+1
+        for i in range(1,rows+1):
+          print ("*" * columns)
+
 
     elif number == "5":
         rows = int(input("Daire için satır sayısını giriniz: "))
-        while r < rows:
+        for i in range(1,rows+1):
+          if i-1<rows/2:
+             print (" " * (rows//2 - i+1) + "*" * (2*i - 1))
 
-             print (":(")
-
-             r = r+1
+          else:
+              print(" " * (i-rows//2-1) + "*" * (rows - i//2))
 
     else:
         print("Geçerli bir değer girmediniz!..")
